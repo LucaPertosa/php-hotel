93d9b7e8a248b363bba9e97bc76009716439b8dd
LucaPertosa <luca.pertosa@hotmail.it>
2023-05-04 15:16:31 +0200

    Add: bonus form con ricerca parcheggio e voto

--- index.php
+++ index.php
@@ -36,6 +36,19 @@
             'distance_to_center' => 50
         ],
     ];
+    if(isset($_GET['parking'])) { 
+        if($_GET['parking'] == "yes") { 
+            $hotels = array_filter($hotels, function($hotel) { 
+                return $hotel['parking'] == true; 
+            }); 
+        };
+    };
+    if(isset($_GET['vote'])) { 
+        $vote = $_GET['vote']; 
+        $hotels = array_filter($hotels, function($hotel) use ($vote) { 
+            return $hotel['vote'] >= $vote; 
+        });
+    };
 ?>
 
 <!DOCTYPE html>
@@ -48,6 +61,30 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
+    <form action="index.php" method="get">
+        <div class="container mt-3 mb-3"> 
+            <div class="row"> 
+                <div class="col-4"> 
+                    <div class="form-group"> 
+                        <label for="parking">Filtro Parcheggio</label> 
+                        <select class="form-control" id="parking" name="parking"> 
+                            <option value="">Tutti</option>
+                            <option value="yes">Parcheggio Disponibile</option>
+                        </select> 
+                    </div> 
+                </div> 
+                <div class="col-4"> 
+                    <div class="form-group"> 
+                        <label for="vote">Filtro Voto</label> 
+                        <input type="number" class="form-control" id="vote" name="vote" step="1" min="1" max="5" placeholder="3"> 
+                    </div> 
+                </div> 
+                <div class="col-2 pt-4"> 
+                    <button type="submit" class="btn btn-primary">Filtra</button> 
+                </div> 
+            </div> 
+        </div>
+    </form>
     <table class="table">
         <thead>
             <tr>
